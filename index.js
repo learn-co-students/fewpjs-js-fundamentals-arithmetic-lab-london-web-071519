4f7a007421ebf3354ec8beed931b60ad9a77d508
Roderick Cardenas <roderickcardenas1@gmail.com>
2019-09-02 14:00:20 +0100

    maths lab

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
 // Write your code here
+//make num1 and num2
+const num1 = 2
+const num2 = 31
+//multiply
+const multiply = num1 * num2
+//randon
+const random = Math.floor( Math.random() * 1000 ) + 1
+//remainder
+const mod = 16 % 6
+//max 20
+const max = Math.max( 5, 10, 15, 20 )
