f39c0d76f75bc6f84d4f017e5761c61a5c6b6bbd
github username <github email address>
2019-09-05 16:01:14 +0100

    Done.

--- index.js
+++ index.js
@@ -2,10 +2,20 @@
 
 // Write your code here
 
-const multiply = ((num1 = 1) * (num2 = 62))
+// the below works:
 
-const random = Math.ceil(Math.random() * 10) 
+// const multiply = ((num1 = 1) * (num2 = 62))
 
-const mod =  9 % 5 
+// const random = Math.ceil(Math.random() * 10) 
 
-const max = Math.max(1,2,3,20)
+// const mod =  9 % 5 
+
+// const max = Math.max(1,2,3,20)
+
+  
+const num1 = 2
+const num2 = 31
+const multiply = num1 * num2
+const random = Math.floor( Math.random() * 1000 ) + 1
+const mod = 16 % 6
+const max = Math.max( 5, 10, 15, 20 )
